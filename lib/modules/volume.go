2c7b4ff1e261736984e09dec237149e62e1d3607
fleaz <mail@felixbreidenstein.de>
2019-10-19 20:05:18 +0200

    volume: Switched from amixer to pamixer

--- lib/modules/volume.go
+++ lib/modules/volume.go
@@ -10,38 +10,22 @@ import (
 // Volume represents the configuration for the volume display block.
 type Volume struct {
 	BlockConfigBase `yaml:",inline"`
-	MixerDevice     string `yaml:"mixer_device"`
-	Channel         string `yaml:"channel"`
+	SinkDevice      string `yaml:"sink_device"`
 }
 
 // UpdateBlock updates the volume display block.
-// Currently, only the ALSA master channel volume is supported.
 func (c Volume) UpdateBlock(b *i3barjson.Block) {
 	b.Color = c.Color
-	fullTextFmt := fmt.Sprintf("%s%%s", c.Label)
-	amixerCmd := "amixer"
-	if c.MixerDevice == "" {
-		c.MixerDevice = "default"
+	fullTextFmt := fmt.Sprintf("%s%%s%%%%", c.Label)
+	pamixerCmd := "pamixer"
+	if c.SinkDevice == "" {
+		c.SinkDevice = "0"
 	}
-	if c.Channel == "" {
-		c.Channel = "Master"
-	}
-	amixerArgs := []string{"-D", c.MixerDevice, "get", c.Channel}
-	out, err := exec.Command(amixerCmd, amixerArgs...).Output()
+	cmdArgs := []string{"--sink", c.SinkDevice, "--get-volume"}
+	out, err := exec.Command(pamixerCmd, cmdArgs...).Output()
 	if err != nil {
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
-		return
+	} else {
+		b.FullText = fmt.Sprintf(fullTextFmt, strings.Trim(string(out), "\n"))
 	}
-	outStr := string(out)
-	iBegin := strings.Index(outStr, "[")
-	if iBegin == -1 {
-		b.FullText = fmt.Sprintf(fullTextFmt, "cannot parse amixer output")
-		return
-	}
-	iEnd := strings.Index(outStr, "]")
-	if iEnd == -1 {
-		b.FullText = fmt.Sprintf(fullTextFmt, "cannot parse amixer output")
-		return
-	}
-	b.FullText = fmt.Sprintf(fullTextFmt, outStr[iBegin+1:iEnd])
 }
